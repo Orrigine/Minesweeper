6b2d5af27d24fcfb98be0f66f513bce7390b5af9
Valentin Faguet <valentin@wallmatt.com>
2023-02-23 13:03:02 +0100

    removing unused files

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -18,37 +18,3 @@ public struct Cell
         public bool exploded;
 
     }
-
-public class TileMap : MonoBehaviour
-{
-    // Start is called before the first frame update
-    [SerializeField] GameObject Original;
-    [SerializeField] GameObject[,] Tilemap = new GameObject[10, 10];
-    
-    
-    // 
-    // void InitTilemap()
-    // {
-    //     for (int i = 0; i < 10; i++)
-    //     {
-    //         for (int j = 0; j < 10; j++)
-    //         {
-    //             Instantiate(Original, new Vector2(i, j), Quaternion.identity);
-    //         }
-    //     }
-    // }
-    // void Start()
-    // {
-    //     InitTilemap();
-    // }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     // if (Time.timeSinceLevelLoad > spawnRate)
-    //     // {
-    //     //     Instantiate(Original, new Vector2(2, -2), Quaternion.identity);
-    //     //     spawnRate += 1f;
-    //     // }
-    // }
-}
